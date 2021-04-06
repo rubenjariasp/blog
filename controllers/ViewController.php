003767a005afb69c1a8411b1ba1a9f4cc7404e21
ruben arias <ruprosperi2@gmail.com>
2021-04-05 21:05:57 -0400

    se separo el aside del archivo principal

--- controllers/ViewController.php
+++ controllers/ViewController.php
@@ -6,6 +6,7 @@ class ViewController{
     public function load_wiew($view){
         require_once $this->views_path .'header.php';
         require_once $this->views_path. $view .'.php';
+        require_once $this->views_path .'aside.php';
         require_once $this->views_path .'footer.php';
     }
 }
