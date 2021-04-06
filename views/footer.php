c80c71d3511087cb7bd1350e02d16be229cb6b8f
ruben arias <ruprosperi2@gmail.com>
2021-04-05 20:25:44 -0400

    Se modularizo la GUI

---
+++ views/footer.php
@@ -0,0 +1,8 @@
+</main>
+
+<footer class="footer">
+    <p>Sitio Elaborado por Rubén Arias. Todos los derechos reservados</p>
+</footer>
+</div>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog de Rubén</title>
+    <link rel="stylesheet" href="public/css/styles.css">
+</head>
+<body>
+<div class="container">
+    <header class="header">
+        <h1 class="header__title">Blog de Rubén</h1>
+        <nav class="header__navbar">
+            <a class="header__links" href="#">Deportes</a>
+            <a class="header__links" href="#">Drama</a>
+            <a class="header__links" href="#">Accion</a>
+            <a class="header__links" href="#">Horror</a>
+        </nav>
+    </header>
+
+    <main class="main">
